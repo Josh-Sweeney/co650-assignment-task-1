971cf39613d11bca4d3643e0c1ceacb55d444b3b
Josh Sweeney <91318891+Josh-Sweeney@users.noreply.github.com>
2023-11-30 10:15:15 +0000

    Make statusToString & typeToString a friend function

--- src/data/issue.cpp
+++ src/data/issue.cpp
@@ -1,44 +1,5 @@
 #include "issue.h"
 
-std::string Issue::statusToString(IssueStatus status)
-{
-    switch (status)
-    {
-    case IssueStatus::none:
-        return "None";
-    case IssueStatus::requirements:
-        return "Requirements";
-    case IssueStatus::backlog:
-        return "Backlog";
-    case IssueStatus::inDevelopment:
-        return "In Development";
-    case IssueStatus::developed:
-        return "Developed";
-    case IssueStatus::uat:
-        return "UAT";
-    case IssueStatus::released:
-        return "Released";
-    default:
-        return "None";
-    }
-}
-
-std::string Issue::typeToString(IssueType type)
-{
-    switch (type)
-    {
-    case IssueType::none:
-        return "None";
-    case IssueType::story:
-        return "Story";
-    case IssueType::bug:
-        return "Bug";
-    case IssueType::epic:
-        return "Epic";
-    default:
-        return "None";
-    }
-}
 
 void Issue::print()
 {
--- src/data/issue.h
+++ src/data/issue.h
@@ -16,9 +16,50 @@ protected:
     IssueStatus status;
     IssueType type;
 
-    // TODO: Consider if this is appropriate here
-    static std::string statusToString(IssueStatus status);
-    static std::string typeToString(IssueType type);
-
     Issue(std::string title, std::string description, IssueStatus status, IssueType type);
+
+private:
+    // TODO: Consider if this is appropriate here
+    static friend std::string statusToString(IssueStatus status);
+    static friend std::string typeToString(IssueType type);
 };
+
+std::string statusToString(IssueStatus status)
+{
+    switch (status)
+    {
+    case IssueStatus::none:
+        return "None";
+    case IssueStatus::requirements:
+        return "Requirements";
+    case IssueStatus::backlog:
+        return "Backlog";
+    case IssueStatus::inDevelopment:
+        return "In Development";
+    case IssueStatus::developed:
+        return "Developed";
+    case IssueStatus::uat:
+        return "UAT";
+    case IssueStatus::released:
+        return "Released";
+    default:
+        return "None";
+    }
+}
+
+std::string typeToString(IssueType type)
+{
+    switch (type)
+    {
+    case IssueType::none:
+        return "None";
+    case IssueType::story:
+        return "Story";
+    case IssueType::bug:
+        return "Bug";
+    case IssueType::epic:
+        return "Epic";
+    default:
+        return "None";
+    }
+}
