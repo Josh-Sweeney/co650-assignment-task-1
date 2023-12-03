ceae2145fb35e419f36593291d8b78fece3baa4a
Josh Sweeney <91318891+Josh-Sweeney@users.noreply.github.com>
2023-12-03 16:10:30 +0000

    Fix typo in enum declaration

--- src/data/issue-status.h
+++ src/data/issue-status.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // An enum class is used here to scope the enumerator
-// Otherwise the following error will occur "error: `none` conflicts with a previous decleration"
+// Otherwise the following error will occur "error: `none` conflicts with a previous declaration"
 enum class IssueStatus {
     none,
     requirements,
